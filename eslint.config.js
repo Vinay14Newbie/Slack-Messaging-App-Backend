304b9e8a78043863985989560610159f28d09e01
Vinay14Newbie <wjust1984@gmail.com>
2024-11-29 21:00:41 +0530

    added db config for mongo

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,6 @@
-import pluginJs from '@eslint/js'
-import simpleImportSort from 'eslint-plugin-simple-import-sort'
-import globals from 'globals'
-
+import pluginJs from '@eslint/js';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
@@ -17,4 +16,4 @@ export default [
     }
   },
   pluginJs.configs.recommended
-]
+];
--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
-import express from 'express'
-import { StatusCodes } from 'http-status-codes'
+import express from 'express';
+import { StatusCodes } from 'http-status-codes';
 
-import { PORT } from './config/serverConfig.js'
+import connectDB from './config/dbConfig.js';
+import { PORT } from './config/serverConfig.js';
 
-const app = express()
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
 
 app.get('/ping', (req, res) => {
   return res.status(StatusCodes.OK).json({
     message: 'pong'
-  })
-})
+  });
+});
 
 app.listen(PORT, () => {
-  console.log('Server is running on port ', PORT)
-})
+  console.log('Server is running on port ', PORT);
+  connectDB()
+});
