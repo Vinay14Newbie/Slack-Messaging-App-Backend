d6133782c89d0d418e14f73a17a45cb5b08be8f8
Vinay14Newbie <wjust1984@gmail.com>
2024-12-13 21:33:23 +0530

    Added some notes about bull-board

--- src/config/bullBoardConfig.js
+++ src/config/bullBoardConfig.js
@@ -1,16 +1,16 @@
-import { createBullBoard } from '@bull-board/api';
-import { BullAdapter } from '@bull-board/api/bullAdapter.js'; //it will bind your bullboard ui with corresponding Queue setup
-import { ExpressAdapter } from '@bull-board/express';
+import { createBullBoard } from '@bull-board/api'; //Initializes the Bull-Board interface for managing queues.
+import { BullAdapter } from '@bull-board/api/bullAdapter.js'; //it will bind your bullboard ui with corresponding Queue setup OR Connects Bull queues to Bull-Board.
+import { ExpressAdapter } from '@bull-board/express'; //Connects Bull-Board to an Express server for hosting the UI.
 
 import mailQueue from '../queues/mailQueue.js';
 import testQueue from '../queues/testQueue.js';
 
-const bullServerAdapter = new ExpressAdapter(); //for connection b/w bull board ui and express
+const bullServerAdapter = new ExpressAdapter(); //for connection b/w bull board ui and express OR This connects Bull-Board's management interface to your Express app.
 bullServerAdapter.setBasePath('/ui');
 
 createBullBoard({
   queues: [new BullAdapter(mailQueue), new BullAdapter(testQueue)], //to understand redis you need BullAdapter
-  serverAdapter: bullServerAdapter
+  serverAdapter: bullServerAdapter //Links the Bull-Board interface to the Express adapter.
 });
 
 export default bullServerAdapter;
