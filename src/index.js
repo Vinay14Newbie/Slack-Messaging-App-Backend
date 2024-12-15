b3eff5083a257074ae77e671faf4b3ad2d6f07a1
Vinay14Newbie <wjust1984@gmail.com>
2024-12-15 15:49:22 +0530

    Added socket.io setup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
 import bullServerAdapter from './config/bullBoardConfig.js';
 import connectDB from './config/dbConfig.js';
@@ -7,6 +9,8 @@ import { isAuthenticated } from './middlewares/authMiddleware.js';
 import apiRouter from './routers/apiRouter.js';
 
 const app = express();
+const server = createServer(app); //it will create a server, cause now we're serving the websocket request on same port
+const io = new Server(server);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -20,7 +24,26 @@ app.get('/ping', isAuthenticated, (req, res) => {
 
 app.use('/api', apiRouter);
 
-app.listen(PORT, () => {
+// 'io.on' listens for the connection event, which triggers every time a client establishes a WebSocket connection to the server.
+io.on('connection', (socket) => {
+  console.log('a user connected ', socket.id);
+
+  //message from client
+  // Listens for a custom event named 'messageFromClient' from the connected client.
+  socket.on('messageFromClient', (data) => {
+    console.log('Message from client: ', data);
+
+    // The io.emit method sends an event called 'new message' to every connected client.
+    io.emit('new message', data.toUpperCase()); //broadcast the message with event name 'new message'
+  });
+
+  // 'socket.emit' targets the client associated with the current socket object (instead of all clients, as with io.emit).
+  // setTimeout(() => {
+  //   socket.emit('message', `This is a message from the server:`);
+  // }, 3000);
+});
+
+server.listen(PORT, () => {
   console.log('Server is running on port ', PORT);
   connectDB();
 });
