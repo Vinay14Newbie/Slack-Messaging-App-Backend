80da03d3ea242e5aa29779e90f3f38cfac568c02
Vinay14Newbie <wjust1984@gmail.com>
2024-12-18 15:25:06 +0530

    Added messages in channel response

--- src/services/channelService.js
+++ src/services/channelService.js
@@ -1,6 +1,7 @@
 import { StatusCodes } from 'http-status-codes';
 
 import channelRepository from '../repositories/channelRepository.js';
+import messageRepository from '../repositories/messageRepository.js';
 import ClientError from '../utils/errors/clientError.js';
 
 const isUserPartOfWorkspace = (workspace, userId) => {
@@ -35,7 +36,24 @@ export const getChannelByIdService = async (channelId, userId) => {
         statusCode: StatusCodes.UNAUTHORIZED
       });
     }
-    return channel;
+    const messages = await messageRepository.getPaginatedMessage(
+      { channelId },
+      1,
+      20
+    );
+
+    console.log('channel in channel service: ', channel);
+
+    // return { ...channel, messages };  //it will also distructure the internal properties altogether
+
+    return {
+      messages,
+      _id: channel._id,
+      name: channel.name,
+      createdAt: channel.createdAt,
+      updatedAt: channel.updatedAt,
+      workspaceId: channel.workspaceId
+    };
   } catch (error) {
     console.log('Get channel by ID service error: ', error);
     throw error;
