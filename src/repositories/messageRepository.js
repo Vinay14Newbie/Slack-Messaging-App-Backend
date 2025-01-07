7a7d2eb237b3dfabb5310368a211fc913060d2aa
Vinay14Newbie <wjust1984@gmail.com>
2025-01-07 22:42:38 +0530

    sort to latest messages

--- src/repositories/messageRepository.js
+++ src/repositories/messageRepository.js
@@ -6,7 +6,7 @@ const messageRepository = {
 
   getPaginatedMessage: async function (messageParams, page, limit) {
     const messages = await Message.find(messageParams)
-      .sort({ createdAt: 1 })
+      .sort({ createdAt: -1 })
       .skip((page - 1) * limit)
       .limit(limit)
       .populate('senderId', 'username email avatar');
