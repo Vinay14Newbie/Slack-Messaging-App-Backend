c548d5dd87e04934b25ae772a4625570a029e858
Vinay14Newbie <wjust1984@gmail.com>
2025-01-10 22:50:38 +0530

    Added presigned logic

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -1,5 +1,7 @@
 import { StatusCodes } from 'http-status-codes';
 
+import { s3 } from '../config/awsConfig.js';
+import { AWS_BUCKET_NAME } from '../config/serverConfig.js';
 import { getPaginatedMessageService } from '../services/messageService.js';
 import {
   customErrorResponse,
@@ -26,5 +28,26 @@ export const getPaginatedMessageController = async (req, res) => {
     return res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
       .json(internalErrorResponse(error));
+  }
+};
+
+export const getPresignedUrlFromAWS = async (req, res) => {
+  try {
+    const url = await s3.getSignedUrlPromise('putObject', {
+      Bucket: AWS_BUCKET_NAME,
+      Key: `${req.user}/${Date.now()}`,
+      Expires: 600
+    });
+    return res
+      .status(StatusCodes.OK)
+      .json(successResponse(url, 'Presigned URL generated successfully'));
+  } catch (err) {
+    console.log('Error in getPresignedUrlFromAWS', err);
+    if (err.statusCode) {
+      return res.status(err.statusCode).json(customErrorResponse(err));
+    }
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json(internalErrorResponse(err));
   }
 };
--- src/routers/v1/messages.js
+++ src/routers/v1/messages.js
@@ -1,9 +1,15 @@
 import express from 'express';
 
-import { getPaginatedMessageController } from '../../controllers/messageController.js';
+import {
+  getPaginatedMessageController,
+  getPresignedUrlFromAWS
+} from '../../controllers/messageController.js';
 import { isAuthenticated } from '../../middlewares/authMiddleware.js';
 
 const router = express.Router();
+
+// In Express, routes are matched in the order they are defined. Since /:channelId is a dynamic route, it will match anything unless a more specific route (like /pre-signed-url) is defined before it.
+router.get('/pre-signed-url', isAuthenticated, getPresignedUrlFromAWS);
 
 router.get('/:channelId', isAuthenticated, getPaginatedMessageController);
 
